f65126e4d2b4411a73c35809de975f094a39b2ba
alimahmoodi <ali.mahmoodi.herisgmail.com>
2022-04-19 15:22:01 +0300

    Update

--- FunctionalVsOOP/oop.js
+++ FunctionalVsOOP/oop.js
@@ -1,5 +1,39 @@
-import Calculator from "./Calculator.js"
+class Calculator {
 
+    Total(x) {
+        var result = x;
+        for (let i = 1; i < x; i++) {
+            result += i;
+        }
+        console.log("Total: " + result);
+    }
+
+
+    Multiply(x) {
+        var result = x;
+        for (let i = 1; i < x; i++) {
+            result *= i;
+        }
+        console.log("Multiply: " + result);
+    }
+
+    Distribution(x) {
+        var result = x;
+        for (let i = 1; i < x; i++) {
+            result /= i;
+        }
+        console.log("Distribution: " + result);
+    }
+
+    Power(x) {
+        var result = x;
+        for (let i = 1; i < x; i++) {
+            result ^= i;
+        }
+        console.log("Power: " + result);
+    }
+
+}
 new Calculator().Total(5)
 new Calculator().Multiply(5)
 new Calculator().Distribution(5)
